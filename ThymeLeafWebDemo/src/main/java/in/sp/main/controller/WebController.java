05de89edd9ac33cc7d6101eb7f79563b3be5bff8
Rakshith K P Shetty <rkpshetty@gmail.com>
2026-06-26 19:19:33 +0530

    SpringBoot Thymeleaf Web Demo

---
+++ ThymeLeafWebDemo/src/main/java/in/sp/main/controller/WebController.java
@@ -0,0 +1,18 @@
+package in.sp.main.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class WebController {
+	@GetMapping("/index")
+	public String index() {
+		return "index";
+}
+	
+	
+	
+
+}
